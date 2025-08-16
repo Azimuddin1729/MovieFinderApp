0fd236207a5609fc3656c108a5d804649ddcf50b
Azimuddin1729 <azimuddin3006@gmail.com>
2025-08-16 20:02:38 +0530

    Added Search Facility

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState,useEffect } from 'react'
 import './App.css'
 import Search from './components/Search';
+import axios from 'axios';
+import { Spinner } from './components/Spinner';
+import { MovieCard } from './components/MovieCard';
 
 
 const API_BASE_URL=
@@ -12,32 +15,78 @@ function App() {
    
   const [searchMovie,setSearchMovie]=useState("");
 
-  const [errorMessage,setErrorMessage]=useState<string | null>(null);
+ 
 
-  const fetchMovies = async () => {
+  const [errorMessage,setErrorMessage]=useState("");
+  const [movieList,setMovieList]=useState([]);
+  const [isLoading,setIsLoading]=useState(false);
+
+  const fetchMovies = async (query='') => {
+    setIsLoading(true); 
+    setErrorMessage(""); 
+
     try {
-     
+       const endpoint= query?
+       `${API_BASE_URL}/search/movie?query=${encodeURI(query)}`:
+       `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+        const response = await axios.get(endpoint, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${API_KEY}`,
+        },
+       });
+
+
+      if(!response.data || response.data.length === 0) {
+        throw new Error("Failed to fetch new movies");
+        setMovieList([]);
+        return;
+      }
+
+      const data = response.data;
+      // console.log(data);
+      setMovieList(data.results);
+
+    
+    } 
+    
+    catch (error) {
       
-    } catch (e) {
-      console.error("Error fetching movies:", e);
-      setErrorMessage("Failed to fetch movies");
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+      
+    }
+
+    finally {
+      setIsLoading(false); 
     }
   };
 
-  useEffect(()=>{
+  
 
-  },[])
- 
+  useEffect(()=>{
+    fetchMovies(searchMovie);
+  },[searchMovie])
+
   return (
     <>
-      <div className="absolute inset-0 bg-center bg-cover bg-[url('/background2.jpg')] z-0" />
-
+   
+      <div
+        className="
+          fixed inset-0 
+          bg-[url('/background2.jpg')] 
+          bg-center bg-cover bg-no-repeat 
+          -z-20
+        "
+      />
+     
       <div className="flex flex-col items-center justify-center text-center pt-12 relative z-10 ">
         
-        <header >
+        <header className="sm:mt-10 mt-5">
             <h1>
              
-              <img  className="w-[180px] sm:w-[240px] md:w-[320px] lg:w-[480px] h-auto rounded-lg shadow-lg object-contain mb-6"
+              <img  className="w-[180px] sm:w-[240px] md:w-[320px] lg:w-[480px]  rounded-lg shadow-lg  mb-6 max-w-lg h-auto object-contain mx-auto drop-shadow-md"
               src="./background_main1.png" alt="Movies Banner"></img>
 
               <div className='text-base sm:text-xl md:text-2xl font-semibold text-amber-50'>
@@ -46,10 +95,32 @@ function App() {
                   </span> You Want To Enjoy Watching With Others
               </div>
             </h1>
+
+            <Search searchTerm={searchMovie} setSearchTerm={setSearchMovie}>
+            </Search>
+
           </header>
 
-          <Search searchTerm={searchMovie} setSearchTerm={setSearchMovie}></Search>
 
+          <section className="space-y-9">
+            <h2 className='mt-2'>
+              All Movies 
+            </h2>
+
+              {isLoading? (
+              
+                <Spinner />
+              ) : errorMessage ?(
+                <p className="text-red-500">{errorMessage}</p>
+
+              ):(   <ul className='grid grid-cols-1 gap-4 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 '>
+                  {movieList.map((movie:any) => (
+                      <MovieCard key={movie.id} movie={movie} />
+                  ))}
+                   </ul>
+                )
+              }
+          </section>
       </div>   
     </>
   )
---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,47 @@
+
+export function MovieCard({movie}:any) {
+     const { title,vote_average,poster_path,release_date, original_language } = movie;
+     return (
+       <div className="bg-dark-100/60 p-4 rounded-3xl shadow-inner shadow-light-200/20 flex flex-col items-center w-[200px]">
+            <img
+                src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : "/no-movie.jpg"}
+                alt={title}
+                className="w-[127px] h-[163px] object-cover rounded-lg shadow-md"
+            />
+
+            <h3 className="mt-4 text-center text-sm w-full truncate text-gray-50">{title}</h3>
+
+            <div className=" mt-2 flex flex-row items-center flex-wrap gap-2">
+                <div className="flex flex-row items-center gap-1">
+                    <img 
+                    className="size-4 object-contain"
+                    src="rating.svg" alt="Rating icon"/>
+                    <p className="font-bold text-base text-white">
+                        {vote_average? vote_average.toFixed(1):"N/A"}
+                    </p>
+
+                   
+                    <span className="text-white text-lg mx-2">•
+                    </span>
+
+                    <p className="capitalize text-gray-100 font-medium text-base">
+                        {original_language}
+                    </p>
+
+                    <span className="text-white text-lg mx-2">•
+                    </span>
+
+                    <p className="text-gray-100 font-medium text-base">
+                        {release_date? release_date.split("-")[0]: "N/A"}
+                    </p>
+
+                </div>
+
+                
+            </div>
+       </div>
+
+     );
+}
+
+
